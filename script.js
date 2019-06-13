88a6ab447db2c173a85534408bc639b491ab92c5
Yerin Cha <lenacha32@gmail.com>
2019-06-13 15:50:06 -0700

    Modify script for k6

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ import { check, sleep } from 'k6';
 
 export const options = {
   vus: 300,
-  duration: '300s',
-  rps: 1000,
+  duration: '30s',
+  rps: 300,
 };
 
 export default function () {
-  const res = http.get(`http://localhost:1111/api/restaurants/${Math.ceil(Math.random() * 10000000)}/info`);
+  const res = http.get(`http://localhost:3002/api/restaurants/${Math.ceil(Math.random() * 9999999 + 1)}/info`);
   check(res, {
     'status was 200': r => r.status === 200,
+    'status was 404': r => r.status === 404,
+    'status was 500': r => r.status === 500,
     'transaction time OK': r => r.timings.duration < 2000,
   });
   // sleep(1);
